949621d81616a54f13e1bc004e355c7f8b9bb882
RijuDjangoGit <rijughoshcse54@gmail.com>
2025-08-20 13:12:11 +0530

    update req.txt

--- lookatme/settings.py
+++ lookatme/settings.py
@@ -49,7 +49,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"}
+}
 ROOT_URLCONF = 'lookatme.urls'
 
 TEMPLATES = [
